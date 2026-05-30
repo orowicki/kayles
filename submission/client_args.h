1d92d95e492983e9fe3743efa5dc7dbc546d34a7
Oskar Rowicki <oskar.rowicki@gmail.com>
2026-05-30 14:02:54 +0200

    Clean up repository

--- submission/client_args.h
+++
@@ -1,8 +0,0 @@
-#ifndef CLIENT_ARGS_H
-#define CLIENT_ARGS_H
-
-#include "client_config.h"
-
-ClientConfig configure_from_args(int argc, char *argv[]);
-
-#endif /* CLIENT_ARGS_H */
--- submission/server_args.h
+++
@@ -1,8 +0,0 @@
-#ifndef SERVER_ARGS_H
-#define SERVER_ARGS_H
-
-#include "server_config.h"
-
-ServerConfig configure_from_args(int argc, char *argv[]);
-
-#endif /* SEVER_ARGS_H */
